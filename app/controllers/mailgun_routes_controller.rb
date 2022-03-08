33157088e71e498ea8972429b36cf89d1cbd55bd
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-03-08 00:38:01 -0500

    Add rejection logging setting

--- app/controllers/mailgun_routes_controller.rb
+++ app/controllers/mailgun_routes_controller.rb
@@ -22,24 +22,40 @@ class MailgunRoutesController < ApplicationController
     end
 
     email_raw = params['body-mime']
+    email_domain = params[:from].split("@")[1]
     retried = false
+    should_log = SiteSetting.mailgun_log_rejections
     spam_detect_method = SiteSetting.mailgun_spam_detection
+    blocked_domains = SiteSetting.mailgun_blocked_domains.split("|")
+    email_rejection_template = "Email from #{params[:from]} rejected because"
+
+    if blocked_domains.include?(email_domain)
+      if should_log
+        Rails.logger.info "#{email_rejection_template} domain is blocked"
+      end
+      return render json: { :error => "Sending domain #{email_domain} is blocked" }, status: 406
+    end
 
     if spam_detect_method != "none"
       spam_flag_header = email_raw.match(/^X-Mailgun-Sflag: (No|Yes)/im)
       spam_score_header = email_raw.match(/^X-Mailgun-Sscore: (-?\d{1,2}\.?\d?)/im)
 
       if not spam_flag_header or not spam_score_header
+        if should_log
+          Rails.logger.info "#{email_rejection_template} spam headers (X-Mailgun-Sflag / X-Mailgun-Sscore) are missing"
+        end
         return render json: { :error => 'Missing spam headers' }, status: 406
       end
 
       dkim_header = email_raw.match(/X-Mailgun-Dkim-Check-Result: (Fail|Pass)/im)
       dkim_exclusions = SiteSetting.dkim_domain_exclusions.split("|")
-      email_domain = params[:from].split("@")[1]
 
       if not dkim_header and
         not dkim_exclusions.include?(email_domain) or
         dkim_header.captures[0].downcase == "fail"
+        if should_log
+          Rails.logger.info "#{email_rejection_template} DKIM validation failed and domain is not excluded"
+        end
         return render json: { :error => 'DKIM did not validate' }, status: 406
       end
 
@@ -49,6 +65,9 @@ class MailgunRoutesController < ApplicationController
       if not spf_header and
         not spf_exclusions.include?(email_domain) or
         spf_header.captures[0].downcase != "pass"
+        if should_log
+          Rails.logger.info "#{email_rejection_template} SPF validation failed and domain is not excluded"
+        end
         return render json: { :error => 'SPF did not validate' }, status: 406
       end
 
@@ -56,6 +75,9 @@ class MailgunRoutesController < ApplicationController
         spam_flag_header.captures[0].downcase == "yes" or
         spam_detect_method == "score" and
         spam_score_header.captures[0].to_f >= SiteSetting.mailgun_spam_score
+        if should_log
+          Rails.logger.info "#{email_rejection_template} #{spam_detect_method == "flag" ? "spam flag was yes": "spam score from Mailgun was #{spam_score_header.captures[0]}, which was equal to or higher than the configured score of #{SiteSetting.mailgun_spam_score.to_s}"}"
+        end
         return render json: { :error => 'Spam detected' }, status: 406
       end
     end
